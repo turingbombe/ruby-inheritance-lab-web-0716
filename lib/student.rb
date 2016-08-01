d2be10f5357d52195b45278e466af3cf3e06f983
Charlie Cook <turingbombe@gmail.com>
2016-08-01 11:32:06 -0400

    inheritance lab completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
 
+  attr_accessor :first_name, :last_name, :knowledge
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(information)
+    knowledge << information
+  end
 end
